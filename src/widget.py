5d658a6bc0ea360a434be4d4be33595128b46290
Roman Gerasimov <rgerasimov58@gmail.com>
2023-10-25 00:31:33 +0300

    add function type_of_cards, return type and mask number of card

---
+++ src/widget.py
@@ -0,0 +1,12 @@
+from masks import number_of_card
+
+
+def type_of_cards(type_and_number: str | list[str]) -> str:
+    split_list = type_and_number.split()
+    only_number = number_of_card(split_list[-1])
+    only_type = split_list[:1]
+    join_only_type = ''.join(only_type)
+    return f'{join_only_type} {only_number}'
+
+
+print(type_of_cards('Maestro 1596837868705199'))
