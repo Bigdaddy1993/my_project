3869517dc28bd699ae70ba0bba246e71fe9e60a4
Roman Gerasimov <rgerasimov58@gmail.com>
2023-10-25 19:53:40 +0300

    renamed function type_of_cards -> type_of_info fix function type_of_info

--- src/widget.py
+++ src/widget.py
@@ -1,29 +1,15 @@
-from masks import number_of_card, number_of_account
+from src.masks import number_of_card, number_of_account
 
 
-def type_of_cards(type_and_number: str , account_and_number) -> str:
-    """Принимает на вход строку информацией тип карты/счета и номер карты/счета
-    Возвращает тип карты/счета и замаскированный номер карты/счета."""
-    split_list = type_and_number.split()
-    split_account = account_and_number.split()
-    only_account = number_of_account(split_account[-1])
-    only_number_account = split_account[:1]
-    join_oneline = "".join(only_number_account)
-    if len(split_list) == 3:
-        only_number = number_of_card(split_list[-1])
-        only_type = split_list[:2]
-        join_only_type = " ".join(only_type)
-        return f"{join_only_type} {only_number} \n{join_oneline} {only_account}"
-    elif len(split_list) == 2:
-        only_number = number_of_card(split_list[-1])
-        only_type = split_list[:1]
-        join_only_type = " ".join(only_type)
-        return f"{join_only_type} {only_number} \n{join_oneline} {only_account}"
-    else:
-        return "не знаю такой номер карты/счета"
+def type_of_info(masking: str) -> str:
+    """Принимает на вход строку с информацией: тип карты/счета и номер карты/счета
+        Возвращает тип карты/счета и замаскированный номер карты/счета."""
+    list_mask = masking.split()
+    joined = ' '.join(list_mask[:-1])
 
-
-print(type_of_cards("Visa 8990922113665229", "Счет 64686473678894779589"))
+    if 'счет' == list_mask[0].lower():
+        return f'{joined} {number_of_account(list_mask[-1])}'
+    return f'{joined} {number_of_card(list_mask[-1])}'
 
 
 def datetime_of_operation(date_and_time: str) -> str:
@@ -34,3 +20,5 @@ def datetime_of_operation(date_and_time: str) -> str:
 
 
 print(datetime_of_operation("2018-07-11T02:26:18.671407"))
+print(type_of_info('Счет 64686473678894779589'))
+print(type_of_info('Visa Platinum 8990922113665229'))
