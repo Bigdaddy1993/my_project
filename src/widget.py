0a8e2a2893a43d789a287c4ea540378d34dedbac
Roman Gerasimov <rgerasimov58@gmail.com>
2023-10-25 13:30:20 +0300

    final function release

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,24 @@
-from masks import number_of_card
+from masks import number_of_card, number_of_account
 
 
-def type_of_cards(type_and_number: str | list[str]) -> str:
-    """Принимает на вход строку информацией тип карты и номер карты
-       Возвращает тип карты и замаскированный номер карты."""
+def type_of_cards(type_and_number: str | list[str], account_and_number) -> str:
+    """Принимает на вход строку информацией тип карты/счета и номер карты/счета
+       Возвращает тип карты/счета и замаскированный номер карты/счета."""
     split_list = type_and_number.split()
-    only_number = number_of_card(split_list[-1])
-    only_type = split_list[:1]
-    join_only_type = ''.join(only_type)
-    return f'{join_only_type} {only_number}'
+    split_account = account_and_number.split()
+    only_account = number_of_account(split_account[-1])
+    only_number_account = split_account[:1]
+    join_oneline = ''.join(only_number_account)
+    if len(split_list) == 3:
+        only_number = number_of_card(split_list[-1])
+        only_type = split_list[:2]
+        join_only_type = ' '.join(only_type)
+        return f'{join_only_type} {only_number} \n{join_oneline} {only_account}'
+    elif len(split_list) == 2:
+        only_number = number_of_card(split_list[-1])
+        only_type = split_list[:1]
+        join_only_type = ' '.join(only_type)
+        return f'{join_only_type} {only_number} \n{join_oneline} {only_account}'
 
 
-print(type_of_cards('Maestro 1596837868705199'))
+print(type_of_cards('Visa 8990922113665229', 'Счет 64686473678894779589'))
