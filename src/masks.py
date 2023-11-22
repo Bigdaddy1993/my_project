22e326217a1a401e4ad10581cef4f10a5161111e
Roman Gerasimov <rgerasimov58@gmail.com>
2023-11-22 10:58:25 +0300

    check isort, flake8, mypy

--- src/masks.py
+++ src/masks.py
@@ -11,9 +11,9 @@ def number_of_card(card_number: str) -> str:
     mask_number = card_number[:6] + (len(card_number[6:-4]) * "*") + card_number[-4:]
     four_numbers_size = 4
     mask_number_card = " ".join(
-        [mask_number[num: num + four_numbers_size] for num in range(0, len(mask_number), four_numbers_size)]
+        [mask_number[num : num + four_numbers_size] for num in range(0, len(mask_number), four_numbers_size)]
     )
-    logger.info(f'Номер карты замаскирован {mask_number_card}')
+    logger.info(f"Номер карты замаскирован {mask_number_card}")
     return mask_number_card
 
 
@@ -26,5 +26,5 @@ def number_of_account(account_number: str) -> str:
 
     mask_number = (len(account_number[:-4]) * "*") + account_number[-4:]
     sliced = mask_number[-6:]
-    logger.info(f'Номер счёта замаскирован {sliced}')
+    logger.info(f"Номер счёта замаскирован {sliced}")
     return sliced
